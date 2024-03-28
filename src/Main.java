d147949252ea50c9b22d739fc3cf9276919e2d7b
TobyQAQ <a0988065874@gmail.com>
2024-03-28 13:43:24 +0800

    240328_2
    1.練習了有參數無回傳值的函式
    2.修改了剛剛函式的命名(以便後續的排列)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
 
-    public static void printme() {
-        System.out.print("我好棒棒");
+    //無參數無回傳值
+    public static void printme1() {
+        System.out.println("我好棒棒");
+    }
+
+    //有參數無回傳值
+    public static void printme2(String a) {
+        System.out.println("你超棒"+ a);
     }
 
     public static void main(String[] args) {
 
-        printme();
+        printme1();
+
+        printme2("kevin");
     }
 }
