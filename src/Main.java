3877084fc2f2ecbb0aec2eaf3751d5548fd0a356
TobyQAQ <a0988065874@gmail.com>
2024-03-28 13:33:25 +0800

    240328_1
    1.練習JAVA的定義

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void printme() {
+        System.out.print("我好棒棒");
+    }
 
+    public static void main(String[] args) {
+
+        printme();
     }
 }
