a0d5a396c3976414708ebdae27b4892072884ebc
TobyQAQ <a0988065874@gmail.com>
2024-03-28 13:58:39 +0800

    240328_3
    1.練習了有回傳值無參數的函式
    2.有回傳值的函示最好的做法是用一個變數來接住這個函式的回傳值

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,24 @@ public class Main {
 
     //有參數無回傳值
     public static void printme2(String a) {
-        System.out.println("你超棒"+ a);
+        System.out.println("你超棒" + a);
     }
+
+    //無參數有回傳值
+    public static String printme3() {
+
+        return "救命";
+    }
+
 
     public static void main(String[] args) {
 
         printme1();
 
         printme2("kevin");
+
+        //如果有回傳值必須要有變數來接回傳的值
+        String a = printme3();
+        System.out.println(a);
     }
 }
